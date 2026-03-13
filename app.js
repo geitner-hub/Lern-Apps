6fb40f503708dc2136e4d830186be8d0f00c5204
geitner-hub <bennigeitner@gmail.com>
2026-03-13 15:33:27 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+body{
+font-family:Arial;
+text-align:center;
+background:#eef;
+}
+
+canvas{
+background:white;
+border:2px solid black;
+margin:20px;
+}
+
+button{
+padding:10px 20px;
+font-size:16px;
+}
+
+input,select{
+padding:8px;
+font-size:16px;
+}
